64d033643f2819b69adb130690fc0ad3650ea7c3
Diego Tribek <46631262+DTribek@users.noreply.github.com>
2019-05-29 21:53:54 -0300

    Add files via upload
    Resolução do notebook e resolução do script caixa_eletronico.py

--- data_science/caixa_eletronico.py
+++ data_science/caixa_eletronico.py
@@ -4,10 +4,39 @@ class ATM():
     """
 
     def process_request(self, input):
-        """
-        ... Develop our code ...
-        """
-        return None
+
+        money_value = int(input['request']['request'])
+
+        n50=0
+        n20=0
+        n50 = money_value // 50
+        response_dict = {}
+        final = {}
+
+        #Check if the requested amount can be maximized with 50 values
+        if ( money_value - n50*50) % 20 == 0: 
+            n20 = (money_value -n50*50) // 20
+            response_dict['20'] = n20
+            response_dict['50'] = n50
+        #Check if the requested value can be reached completing with 20's
+        elif (money_value - (n50-1)*50) % 20 == 0:
+            n50 = n50 -1
+            n20 = (money_value -n50*50) // 20
+            response_dict['20'] = n20
+            response_dict['50'] = n50
+        #Value not compatible
+        else:
+            response_dict['error'] = 'Sua mensagem de erro'
+
+        response = {}
+        response['response'] = response_dict
+        
+        input['requester'] = input.pop('request')
+        input['requester']['requested'] = input['requester'].pop('request')
+        final = dict(input)
+        final.update(response)
+
+        return final
 
 
 if __name__ == "__main__":
@@ -26,14 +55,14 @@ if __name__ == "__main__":
     for idx, sample in enumerate(test_samples):
         input    = sample.get("input")
         correct_output = sample.get("output")
-
-        processed_output = atm.process_request(inupt)
-
+        processed_output = atm.process_request(input)
+        
         # Checks if the result is correct
         if processed_output == correct_output:
             count_success += 1
             print("Sample # {} - Success".format(idx))
-        else
+        # 
+        else:
             print("Sample # {} - Error".format(idx))
             count_fail += 1
     
